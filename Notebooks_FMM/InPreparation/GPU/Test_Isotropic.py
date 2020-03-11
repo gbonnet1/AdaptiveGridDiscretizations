42757a6718888f178e0369d770eb7fb2b0333630
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-03-11 22:27:48 +0100

    Update Test_Isotropic.py

--- Notebooks_FMM/InPreparation/GPU/Test_Isotropic.py
+++ Notebooks_FMM/InPreparation/GPU/Test_Isotropic.py
@@ -2,4 +2,18 @@ import sys; sys.path.insert(0,"../../..") # Allow import of agd from parent dire
 
 from agd import HFMUtils
 from agd.HFMUtils import HFM_CUDA
-import numpy as np
+import cupy as xp
+
+
+hfmIn = HFMUtils.dictIn({
+    'model':'Isotropic2',
+    'arrayOrdering':'RowMajor',
+    'seeds':[[0,0]],
+    'kernel':"dummy",
+    'solver':'globalIteration',
+})
+hfmIn.SetRect([[-1,1],[-1,1]],dimx=8)
+hfmIn['cost'] = xp.ones(hfmIn['dims'].astype(int),dtype='float32')
+
+
+hfmOut = hfmIn.RunGPU(returns='in_raw')
