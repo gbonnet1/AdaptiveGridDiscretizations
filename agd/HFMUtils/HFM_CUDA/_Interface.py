bcc002ee3b1a95ca98cfa0556a7204415dbc8a5a
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-04-07 21:55:29 +0200

    Further split the implementation of HFM_CUDA Interface class

--- agd/HFMUtils/HFM_CUDA/_Interface.py
+++ agd/HFMUtils/HFM_CUDA/_Interface.py
@@ -4,7 +4,6 @@
 import numpy as np
 import os
 import time
-import copy
 from collections import OrderedDict
 
 from . import kernel_traits
@@ -17,8 +16,6 @@ from . import _solvers
 from . import _GetGeodesics
 from . import _PostProcess
 
-from .. import Grid
-from ... import FiniteDifferences as fd
 from ... import AutomaticDifferentiation as ad
 from ... import Metrics
 from ... import LinearParallel as lp
@@ -108,8 +105,9 @@ class Interface(object):
 		self.SetGeometry()
 		self.SetValuesArray()
 		self.SetKernel()
-		self.SetSolver()
+		self.Solve()
 		self.PostProcess()
+		self.SolveAD()
 		self.GetGeodesics()
 		self.FinalCheck()
 
@@ -167,246 +165,8 @@ class Interface(object):
 
 		self.traits = traits
 
-	def SetGeometry(self):
-		if self.verbosity>=1: print("Prepating the domain data (shape,metric,...)")
-
-		# Domain shape and grid scale
-		self.shape = tuple(self.GetValue('dims',
-			help="dimensions (shape) of the computational domain").astype(int))
-
-		self.periodic_default = (False,False,True) if self.isCurvature else (False,)*self.ndim
-		self.periodic = self.GetValue('periodic',default=self.periodic_default,
-			help="Apply periodic boundary conditions on some axes")
-		self.shape_o = tuple(misc.round_up(self.shape,self.shape_i))
-		if self.bound_active_blocks is True: 
-			self.bound_active_blocks = 12*np.prod(self.shape_o) / np.max(self.shape_o)
-		
-		if self.HasValue('gridScale'):
-			self.h = self.GetValue('gridScale',array_float=True,
-				help="Scale of the computational grid")
-			# Gridscale for periodic dimension, in the curvature case
-			if self.isCurvature: 
-				self.h_per = self.caster(2.*np.pi / self.shape[2] )
-				self.h = self.caster((self.h,self.h,self.h_per))
-		else:
-			self.h = self.GetValue('gridScales',array_float=True,
-				help="Axis independent scales of the computational grid")
-
-		self.h_broadcasted = np.broadcast_to(self.h,(self.ndim,))
-		self.h_broadcasted = np.broadcast_to(np.reshape(self.h_broadcasted,
-			(self.ndim,)+(1,)*self.ndim),(self.ndim,)+self.shape)
-
-		self.drift = self.GetValue('drift', default=None, verbosity=3, array_float=True,
-			help="Drift introduced in the eikonal equation, becoming F(grad u - drift)=1")
-
-		# Get the metric or cost function
-		self.cost_based = self.model.startswith('Isotropic') or self.isCurvature
-		if self.cost_based:
-			self.metric = self.GetValue('cost',None,verbosity=3,
-				help="Cost function for the minimal paths. cost = 1/speed.")
-			self.dualMetric = self.GetValue('speed',None,verbosity=3,
-				help="Speed function for the minimal paths. speed = 1/cost.")
-		else:
-			self.metric = self.GetValue('metric',default=None,verbosity=3,
-				help="Metric of the minimal path model")
-			self.dualMetric = self.GetValue('dualMetric',default=None,verbosity=3,
-				help="Dual metric of the minimal path model")
-
-		# Import from HFM format, and make copy, if needed
-		if self.model.startswith('Isotropic') or self.isCurvature: 
-			metricClass = Metrics.Isotropic
-		elif self.model.startswith('Riemann'): metricClass = Metrics.Riemann
-		elif self.model.startswith('Rander') : metricClass = Metrics.Rander
-
-		overwriteMetric = self.GetValue("overwriteMetric",default=False,
-				help="Allow overwriting the metric or dualMetric")
-
-		if ad.cupy_generic.isndarray(self.metric) or np.isscalar(self.metric): 
-			self.metric = metricClass.from_HFM(self.metric)
-		elif not overwriteMetric:
-			self.metric = copy.deepcopy(self.metric)
-		if ad.cupy_generic.isndarray(self.dualMetric) or np.isscalar(self.dualMetric): 
-			self.dualMetric = metricClass.from_HFM(self.dualMetric)
-		elif not overwriteMetric:
-			self.dualMetric = copy.deepcopy(self.dualMetric)
-
-		# Rescale 
-		h_base = self.h[0] if self.isCurvature else self.h
-		if self.metric is not None: 	self.metric =     self.metric.rescale(1/h_base)
-		if self.dualMetric is not None: self.dualMetric = self.dualMetric.rescale(h_base)
-		if self.drift is not None: self.drift*=self.h_broadcasted
-
-		self.costVariation = self.GetValue('costVariation',default=None,
-			help = "First order variation of the cost function "
-			"(defined as 1 in the case of an anisotropic metric)")
-		if self.cost_based: 
-			# These models internally only use the cost function, not the speed function
-			if self.metric is None: self.metric = self.dualMetric.dual()
-			self.dualMetric = None
-			if ad.is_ad(self.metric.cost):
-				assert self.costVariation is None
-				self.costVariation = self.metric.cost.coef
-				self.metric = Metrics.Isotropic(self.metric.cost.value)
-			# Internally, we always use a relative cost variation
-			if self.costVariation is not None:
-				self.costVariation/=self.xp.expand_dims(self.metric.cost,axis=-1)
-
-		if self.isCurvature:
-			self.xi = self.GetValue('xi',
-				help="Cost of rotation for the curvature penalized models")
-			self.kappa = self.GetValue('kappa',default=0.,
-				help="Rotation bias for the curvature penalized models")
-			self.theta = self.GetValue('theta',default=0.,verbosity=3,
-				help="Deviation from horizontality, for the curvature penalized models")
-
-			self.xi *= self.h_per
-			self.kappa /= self.h_per
-
-			self.geom = ad.array([e for e in 
-				(self.metric.cost,self.xi,self.kappa,self.theta) if not np.isscalar(e)])
-
-		else: # not self.isCurvature
-			if self.model.startswith('Isotropic'):
-				self.geom = self.metric.cost
-			elif self.model.startswith('Riemann'):
-				if self.dualMetric is None: self.dualMetric = self.metric.dual()
-				self.geom = self.dualMetric.flatten()
-			elif self.model.startswith('Rander'):
-				if self.metric is None: self.metric = self.dualMetric.dual()
-				self.geom = Metrics.Riemann(self.metric.m).dual().flatten()
-				if self.drift is None: self.drift = self.metric.w
-				else: self.drift += self.metric.w
-
-			# TODO : remove. No need to create this grid for our interpolation
-			grid = ad.array(np.meshgrid(*(self.xp.arange(s,dtype=self.float_t) 
-				for s in self.shape), indexing='ij')) # Adimensionized coordinates
-			self.metric.set_interpolation(grid,periodic=self.periodic) # First order interpolation
-
-
-		self.block['geom'] = misc.block_expand(fd.as_field(self.geom,self.shape),
-			self.shape_i,mode='constant',constant_values=self.xp.inf,contiguous=True)
-		if self.drift is not None:
-			self.block['drift'] = misc.block_expand(fd.as_field(self.drift,self.shape),
-				self.shape_i,mode='constant',constant_values=self.xp.nan,contiguous=True)
-
-		tol_msg = "Convergence tolerance for the fixed point solver"
-		mean_cost_magnitude_msg = ("Upper bound on the magnitude of the cost function, "
-		"or equivalent quantity for a metric, used to set the 'tol' and 'minChg_delta_min' "
-		"parameters of the eikonal solver")
-
-		tol = self.GetValue('tol',default="_Dummy",help=tol_msg)
-		if isinstance(tol,str) and tol=="_Dummy":
-			if self.metric is None: self.metric=self.dualMetric.dual()
-			mean_cost_bound = float(np.mean(self.metric.cost_bound()))
-			float_resolution = np.finfo(self.float_t).resolution
-			tol = mean_cost_bound * float_resolution * 5.
-			if not self.multiprecision: tol*=np.sum(self.shape)
-			self.hfmOut['keys']['defaulted']['tol']=tol
-		self.tol = self.float_t(tol)
-
-		if self.bound_active_blocks:
-			self.minChg_delta_min = self.GetValue('minChg_delta_min',default=float(self.h)/10.,
-				help="Minimal threshold increase for bound_active_blocks variants")
-
-
-		# geometrical data related with geodesics geodesics
-		self.exportGeodesicFlow = self.GetValue('exportGeodesicFlow',default=False,
-			help="Export the upwind geodesic flow (direction of the geodesics)")
-		self.tips = self.GetValue('tips',default=None,array_float=True,
-			help="Tips from which to compute the minimal geodesics")
-		if self.isCurvature:
-			self.unorientedTips=self.GetValue('unorientedTips',default=None,array_float=True,
-				help="Compute a geodesic from the most favorable orientation")
-		self.hasTips = (self.tips is not None 
-			or self.isCurvature and self.unorientedTips is not None)
-
-
-	def SetValuesArray(self):
-		if self.verbosity>=1: print("Preparing the values array (setting seeds,...)")
-		xp = self.xp
-		values = xp.full(self.shape,xp.inf,dtype=self.float_t)
-		self.seeds = self.GetValue('seeds',
-			help="Points from where the front propagation starts",array_float=True)
-		assert self.seeds.ndim==2 and self.seeds.shape[1]==self.ndim
-		self.seeds = Grid.PointFromIndex(self.hfmIn,self.seeds,to=True) # Adimensionize seed position
-		if len(self.seeds)==1: self.seed=self.seeds[0]
-		seedValues = xp.zeros(len(self.seeds),dtype=self.float_t)
-		seedValues = self.GetValue('seedValues',default=seedValues,
-			help="Initial value for the front propagation",array_float=True)
-		if not ad.is_ad(seedValues):
-			seedValueVariation = self.GetValue('seedValueVariation',default=None,
-				help="First order variation of the seed values",array_float=True)
-			if seedValueVariation is not None:
-				denseAD_cupy = ad.cupy_generic.cupy_rebase(ad.Dense.denseAD)
-				seedValues = denseAD_cupy.new(seedValues,seedValueVariation.T)
-		seedRadius = self.GetValue('seedRadius',default=0.,
-			help="Spread the seeds over a radius given in pixels, so as to improve accuracy.")
-
-
-		if seedRadius==0.:
-			seedIndices = np.round(self.seeds).astype(int)
-			values[tuple(seedIndices.T)] = seedValues
-			self.seedValues = seedValues
-			self.seedIndices = seedIndices
-		else:
-			neigh = Grid.GridNeighbors(self.hfmIn,self.seed,seedRadius) # Geometry last
-			r = seedRadius 
-			aX = [xp.arange(int(np.floor(ci-r)),int(np.ceil(ci+r)+1)) for ci in self.seed]
-			neigh =  ad.stack(xp.meshgrid( *aX, indexing='ij'),axis=-1)
-			neigh = neigh.reshape(-1,neigh.shape[-1])
-			neighValues = seedValues.repeat(len(neigh)//len(self.seeds)) # corrected below
-
-			# Select neighbors which are close enough
-			neigh = neigh[ad.Optimization.norm(neigh-self.seed,axis=-1) < r]
-
-			# Periodize, and select neighbors which are in the domain
-			nper = np.logical_not(self.periodic)
-			inRange = np.all(np.logical_and(-0.5<=neigh[:,nper],
-				neigh[:,nper]<xp.array(self.shape)[nper]-0.5),axis=-1)
-			neigh = neigh[inRange,:]
-			neighValues = neighValues[inRange,:]
-			
-			diff = (neigh - self.seed).T # Geometry first
-#			neigh[:,self.periodic] = neigh[:,self.periodic] % self.shape[self.periodic]
-			metric0 = self.Metric(self.seed)
-			metric1 = self.Metric(neigh.T)
-			self.seedValues = neighValues+0.5*(metric0.norm(diff) + metric1.norm(diff))
-			self.seedIndices = neigh
-			values[tuple(self.seedIndices.T)] = ad.remove_ad(self.seedValues)
-
-		block_values = misc.block_expand(values,self.shape_i,
-			mode='constant',constant_values=np.inf,contiguous=True)
-
-		# Tag the seeds
-		if np.prod(self.shape_i)%8!=0:
-			raise ValueError('Product of shape_i must be a multiple of 8')
-		self.seedTags = xp.isfinite(values)
-		block_seedTags = misc.block_expand(self.seedTags,self.shape_i,
-			mode='constant',constant_values=True,contiguous=True)
-		block_seedTags = misc.packbits(block_seedTags,bitorder='little')
-		block_seedTags = block_seedTags.reshape( self.shape_o + (-1,) )
-		block_values[xp.isnan(block_values)] = xp.inf
-
-		self.block.update({'values':block_values,'seedTags':block_seedTags})
-
-		# Handle multiprecision
-		if self.multiprecision:
-			block_valuesq = xp.zeros(block_values.shape,dtype=self.int_t)
-			self.block.update({'valuesq':block_valuesq})
-
-		if self.strict_iter_o:
-			self.block['valuesNext']=block_values.copy()
-			if self.multiprecision:
-				self.block['valuesqNext']=block_valuesq.copy()
-
-		if self.bound_active_blocks:
-			minChg = xp.full(self.shape_o,np.inf,dtype=self.float_t)
-			self.block['minChgPrev_o'] = minChg
-			self.block['minChgNext_o'] = minChg.copy()
-
-		for key,value in self.block.items():
-			if value.dtype.type not in (self.float_t,self.int_t,np.uint8):
-				raise ValueError(f"Inconsistent type {value.dtype.type} for key {key}")
+	SetGeometry = _PreProcess.SetGeometry
+	SetValuesArray = _PreProcess.SetValuesArray
 
 	def SetModuleConstant(self,*args,module="Eikonal",**kwargs):
 		if isinstance(module,str):
@@ -544,7 +304,7 @@ class Interface(object):
 
 		return kernel_args
 
-	def SetSolver(self):
+	def Solve(self):
 		if self.verbosity>=1: print("Setup and run the eikonal solver")
 		solver = self.GetValue('solver',default='AGSI',help="Choice of fixed point solver")
 		self.nitermax_o = self.GetValue('nitermax_o',default=2000,
@@ -591,6 +351,7 @@ class Interface(object):
 
 	
 	PostProcess = _PostProcess.PostProcess
+	SolveAD = _PostProcess.SolveAD
 	GetGeodesics = _GetGeodesics.GetGeodesics
 	
 	def FinalCheck(self):
--- agd/HFMUtils/HFM_CUDA/_PostProcess.py
+++ agd/HFMUtils/HFM_CUDA/_PostProcess.py
@@ -1,3 +1,6 @@
+# Copyright 2020 Jean-Marie Mirebeau, University Paris-Sud, CNRS, University Paris-Saclay
+# Distributed WITHOUT ANY WARRANTY. Licensed under the Apache License, Version 2.0, see http://www.apache.org/licenses/LICENSE-2.0
+
 import numpy as np
 from . import misc
 from . import kernel_traits
@@ -7,102 +10,105 @@ from ... import LinearParallel as lp
 from ... import AutomaticDifferentiation as ad
 from ...AutomaticDifferentiation import numpy_like as npl
 
+# This file implements some member functions of the Interface class of HFM_CUDA
+
 def PostProcess(self):
-		if self.verbosity>=1: print("Post-Processing")
-		values = misc.block_squeeze(self.block['values'],self.shape)
-		if self.multiprecision:
-			valuesq = misc.block_squeeze(self.block['valuesq'],self.shape)
-			if self.GetValue('values_float64',default=False,
-				help="Export values using the float64 data type"):
-				float64_t = np.dtype('float64').type
-				self.hfmOut['values'] = (values.astype(float64_t) 
-					+ float64_t(self.multip_step) * valuesq)
-			else:
-				self.hfmOut['values'] = (values + valuesq.astype(self.float_t)*self.multip_step)
+	if self.verbosity>=1: print("Post-Processing")
+	values = misc.block_squeeze(self.block['values'],self.shape)
+	if self.multiprecision:
+		valuesq = misc.block_squeeze(self.block['valuesq'],self.shape)
+		if self.GetValue('values_float64',default=False,
+			help="Export values using the float64 data type"):
+			float64_t = np.dtype('float64').type
+			self.hfmOut['values'] = (values.astype(float64_t) 
+				+ float64_t(self.multip_step) * valuesq)
 		else:
-			self.hfmOut['values'] = values
+			self.hfmOut['values'] = (values + valuesq.astype(self.float_t)*self.multip_step)
+	else:
+		self.hfmOut['values'] = values
 
-		# Compute the geodesic flow, if needed, and related quantities
-		shape_oi = self.shape_o+self.shape_i
-		nact = kernel_traits.nact(self)
-		ndim = self.ndim
+	# Compute the geodesic flow, if needed, and related quantities
+	shape_oi = self.shape_o+self.shape_i
+	nact = kernel_traits.nact(self)
+	ndim = self.ndim
 
-		if self.flow_traits.get('flow_weights_macro',False):
-			self.flow_kernel_argnames.append('flow_weights')
-			self.block['flow_weights'] = self.xp.empty((nact,)+shape_oi,dtype=self.float_t)
-		if self.flow_traits.get('flow_weightsum_macro',False):
-			self.flow_kernel_argnames.append('flow_weightsum')
-			self.block['flow_weightsum'] = self.xp.empty(shape_oi,dtype=self.float_t)
-		if self.flow_traits.get('flow_offsets_macro',False):
-			self.flow_kernel_argnames.append('flow_offsets')
-			self.block['flow_offsets'] = self.xp.empty((ndim,nact,)+shape_oi,dtype=np.int8)
-		if self.flow_traits.get('flow_indices_macro',False):
-			self.flow_kernel_argnames.append('flow_indices')
-			self.block['flow_indices'] = self.xp.empty((nact,)+shape_oi,dtype=self.int_t)
-		if self.flow_traits.get('flow_vector_macro',False):
-			self.flow_kernel_argnames.append('flow_vector')
-			self.block['flow_vector'] = self.xp.ones((ndim,)+shape_oi,dtype=self.float_t)
+	if self.flow_traits.get('flow_weights_macro',False):
+		self.flow_kernel_argnames.append('flow_weights')
+		self.block['flow_weights'] = self.xp.empty((nact,)+shape_oi,dtype=self.float_t)
+	if self.flow_traits.get('flow_weightsum_macro',False):
+		self.flow_kernel_argnames.append('flow_weightsum')
+		self.block['flow_weightsum'] = self.xp.empty(shape_oi,dtype=self.float_t)
+	if self.flow_traits.get('flow_offsets_macro',False):
+		self.flow_kernel_argnames.append('flow_offsets')
+		self.block['flow_offsets'] = self.xp.empty((ndim,nact,)+shape_oi,dtype=np.int8)
+	if self.flow_traits.get('flow_indices_macro',False):
+		self.flow_kernel_argnames.append('flow_indices')
+		self.block['flow_indices'] = self.xp.empty((nact,)+shape_oi,dtype=self.int_t)
+	if self.flow_traits.get('flow_vector_macro',False):
+		self.flow_kernel_argnames.append('flow_vector')
+		self.block['flow_vector'] = self.xp.ones((ndim,)+shape_oi,dtype=self.float_t)
 
-		self.flow_needed = any(self.flow_traits.get(key+"_macro",False) for key in 
-			('flow_weights','flow_weightsum','flow_offsets','flow_indices','flow_vector'))
-		if self.flow_needed: _solvers.global_iteration(self,solver=False)
+	self.flow_needed = any(self.flow_traits.get(key+"_macro",False) for key in 
+		('flow_weights','flow_weightsum','flow_offsets','flow_indices','flow_vector'))
+	if self.flow_needed: _solvers.global_iteration(self,solver=False)
 
-		self.flow = {}
-		for key in self.block:
-			if key.startswith('flow_'):
-				self.flow[key]=misc.block_squeeze(self.block[key],self.shape,contiguous=True)
+	self.flow = {}
+	for key in self.block:
+		if key.startswith('flow_'):
+			self.flow[key]=misc.block_squeeze(self.block[key],self.shape,contiguous=True)
 
-		if self.model.startswith('Rander') and 'flow_vector' in self.flow:
-			if self.dualMetric is None: self.dualMetric = self.metric.dual()
-			flow_orig = self.flow['flow_vector']
-			m = fd.as_field(self.metric.m,self.shape,depth=2)
-			w = fd.as_field(self.metric.w,self.shape,depth=1)
-			eucl_gradient = lp.dot_AV(m,flow_orig)+w
-			flow = self.dualMetric.gradient(eucl_gradient)
-			flow[np.isnan(flow)]=0. # Vanishing flow yields nan after gradient
-			flow = self.xp.ascontiguousarray(flow)
-			self.flow['flow_vector_orig'],self.flow['flow_vector'] = flow_orig,flow
+	if self.model.startswith('Rander') and 'flow_vector' in self.flow:
+		if self.dualMetric is None: self.dualMetric = self.metric.dual()
+		flow_orig = self.flow['flow_vector']
+		m = fd.as_field(self.metric.m,self.shape,depth=2)
+		w = fd.as_field(self.metric.w,self.shape,depth=1)
+		eucl_gradient = lp.dot_AV(m,flow_orig)+w
+		flow = self.dualMetric.gradient(eucl_gradient)
+		flow[np.isnan(flow)]=0. # Vanishing flow yields nan after gradient
+		flow = self.xp.ascontiguousarray(flow)
+		self.flow['flow_vector_orig'],self.flow['flow_vector'] = flow_orig,flow
 
-		if self.exportGeodesicFlow:
-			self.hfmOut['flow'] = - self.flow['flow_vector'] * self.h_broadcasted
+	if self.exportGeodesicFlow:
+		self.hfmOut['flow'] = - self.flow['flow_vector'] * self.h_broadcasted
 
-		if self.forward_ad or self.reverse_ad:
-			spmod=self.xp.cupyx.scipy.sparse
-			xp=self.xp
-			diag = self.flow['flow_weightsum'].copy() # diagonal preconditioner
-			self.boundary = diag==0. #seeds, or walls, or out of domain
-			diag[self.boundary]=1.
-			coef = xp.concatenate((xp.expand_dims(diag,axis=0),
-					-self.flow['flow_weights']),axis=0)
-			diag_precond=True
-			if diag_precond: coef/=diag
-			size_tot = np.prod(self.shape) # Not same as solver size_tot
-			rg = xp.arange(size_tot).reshape((1,)+self.shape)
-			row = self.xp.broadcast_to(rg,coef.shape)
-			col = xp.concatenate((rg,self.flow['flow_indices']),axis=0)
+def SolveAD(self)
+	if self.forward_ad or self.reverse_ad:
+		spmod=self.xp.cupyx.scipy.sparse
+		xp=self.xp
+		diag = self.flow['flow_weightsum'].copy() # diagonal preconditioner
+		self.boundary = diag==0. #seeds, or walls, or out of domain
+		diag[self.boundary]=1.
+		coef = xp.concatenate((xp.expand_dims(diag,axis=0),
+				-self.flow['flow_weights']),axis=0)
+		diag_precond=True
+		if diag_precond: coef/=diag
+		size_tot = np.prod(self.shape) # Not same as solver size_tot
+		rg = xp.arange(size_tot).reshape((1,)+self.shape)
+		row = self.xp.broadcast_to(rg,coef.shape)
+		col = xp.concatenate((rg,self.flow['flow_indices']),axis=0)
 
-			self.triplets = (npl.flat(coef),(npl.flat(row),npl.flat(col))) 
-			self.spmat = spmod.coo_matrix(self.triplets)
+		self.triplets = (npl.flat(coef),(npl.flat(row),npl.flat(col))) 
+		self.spmat = spmod.coo_matrix(self.triplets)
 
-		if self.forward_ad:
-			if self.costVariation is None:
-				self.costVariation = self.xp.zeros(self.shape+self.seedValues.size_ad,
-					dtype=self.float_t)
-			rhs=self.costVariation 
-			if ad.is_ad(self.seedValues):
-				rhs[tuple(self.seedIndices.T)] = self.seedValues.coef
+	if self.forward_ad:
+		if self.costVariation is None:
+			self.costVariation = self.xp.zeros(self.shape+self.seedValues.size_ad,
+				dtype=self.float_t)
+		rhs=self.costVariation 
+		if ad.is_ad(self.seedValues):
+			rhs[tuple(self.seedIndices.T)] = self.seedValues.coef
 #			rhs/=xp.expand_dims(diag,axis=-1)
-			rhs=rhs.reshape(size_tot,-1)
+		rhs=rhs.reshape(size_tot,-1)
 
-			# Solve the linear system
-			csrmat = self.spmat.tocsr()
-			# In contrast with scipy, lsqr must do one solve per entry. 
-			# Note : lsqr also assumes rhs contiguity
-			self.forward_solutions = [ 
-				spmod.linalg.lsqr(csrmat,self.xp.ascontiguousarray(r)) for r in rhs.T] 
-			self.hfmOut['valueVariation'] = self.xp.stack(
-				[s[0].reshape(self.shape) for s in self.forward_solutions],axis=-1) 
+		# Solve the linear system
+		csrmat = self.spmat.tocsr()
+		# In contrast with scipy, lsqr must do one solve per entry. 
+		# Note : lsqr also assumes rhs contiguity
+		self.forward_solutions = [ 
+			spmod.linalg.lsqr(csrmat,self.xp.ascontiguousarray(r)) for r in rhs.T] 
+		self.hfmOut['valueVariation'] = self.xp.stack(
+			[s[0].reshape(self.shape) for s in self.forward_solutions],axis=-1) 
 
-		if self.reverse_ad:
-			rhs = self.GetValue('sensitivity',help='Reverse automatic differentiation')
-			hfmOut['valueSensitivity'] = spmod.linalg.lsqr(self.spmat.T.tocsr(),rhs)
+	if self.reverse_ad:
+		rhs = self.GetValue('sensitivity',help='Reverse automatic differentiation')
+		hfmOut['valueSensitivity'] = spmod.linalg.lsqr(self.spmat.T.tocsr(),rhs)
