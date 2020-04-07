66eb74f30934113fed0d9931e73132ab7da8944b
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-04-07 09:45:26 +0200

    Deferred implementation of GetGeodesics

--- agd/HFMUtils/HFM_CUDA/interface.py
+++ agd/HFMUtils/HFM_CUDA/interface.py
@@ -8,11 +8,15 @@ import copy
 from collections import OrderedDict
 
 from . import kernel_traits
-from . import solvers
 from . import misc
 from . import cupy_module_helper
 from .cupy_module_helper import GetModule
 from .import inf_convolution
+
+# Deferred implementation of Interface member functions
+from . import _solvers 
+from . import _GetGeodesics
+
 from .. import Grid
 from ... import FiniteDifferences as fd
 from ... import AutomaticDifferentiation as ad
@@ -244,7 +248,8 @@ class Interface(object):
 				self.costVariation = self.metric.cost.coef
 				self.metric = Metrics.Isotropic(self.metric.cost.value)
 			# Internally, we always use a relative cost variation
-			self.costVariation/=self.xp.expand_dims(self.metric.cost,axis=-1)
+			if self.costVariation is not None:
+				self.costVariation/=self.xp.expand_dims(self.metric.cost,axis=-1)
 
 		if self.isCurvature:
 			self.xi = self.GetValue('xi',
@@ -557,9 +562,9 @@ class Interface(object):
 
 		solver_start_time = time.time()
 		if solver=='global_iteration':
-			niter_o = solvers.global_iteration(self)
+			niter_o = _solvers.global_iteration(self)
 		elif solver in ('AGSI','adaptive_gauss_siedel_iteration'):
-			niter_o = solvers.adaptive_gauss_siedel_iteration(self)
+			niter_o = _solvers.adaptive_gauss_siedel_iteration(self)
 		else:
 			raise ValueError(f"Unrecognized solver : {solver}")
 
@@ -616,7 +621,7 @@ class Interface(object):
 
 		self.flow_needed = any(self.flow_traits.get(key+"_macro",False) for key in 
 			('flow_weights','flow_weightsum','flow_offsets','flow_indices','flow_vector'))
-		if self.flow_needed: solvers.global_iteration(self,solver=False)
+		if self.flow_needed: _solvers.global_iteration(self,solver=False)
 
 		self.flow = {}
 		for key in self.block:
@@ -663,120 +668,8 @@ class Interface(object):
 			rhs = self.GetValue('sensitivity',help='Reverse automatic differentiation')
 			hfmOut['valueSensitivity'] = spmod.linalg.lsqr(self.spmat.T.tocsr(),rhs)
 
-	def GetGeodesics(self):
-		if not self.hasTips: return
-
-		# Set the kernel traits
-		geodesic_step = self.GetValue('geodesic_step',default=0.25,
-			help='Step size, in pixels, for the geodesic ODE solver')
-
-		geodesic_hlen = int(4*np.sqrt(self.ndim)/geodesic_step)
-		geodesic_hlen = self.GetValue('geodesic_hlen',default=geodesic_hlen,
-			help="History length for the geodesic solver, for termination error criteria")
-
-		geodesic_traits = {
-			'hlen':geodesic_hlen,
-			'eucl_delay':geodesic_hlen-1,
-			'nymin_delay':geodesic_hlen-1,
-			'EuclT':np.uint8,
-			}
-		if any(self.periodic): geodesic_traits['periodic'] = self.periodic
-		geodesic_traits.update(self.GetValue('geodesic_traits',default=geodesic_traits,
-			help='Traits for the geodesic backtracking kernel') )
-		geodesic_traits.update({ # Non-negotiable
-			'ndim':self.ndim,
-			'Int':self.int_t,
-			'Scalar':self.float_t})
-		self.geodesic_traits=geodesic_traits
-
-		# Get the module
-		self.geodesic_source = cupy_module_helper.traits_header(geodesic_traits,
-			join=True,integral_max=True) + "\n"
-		cuoptions = ("-default-device", f"-I {self.cuda_path}") 
-		self.geodesic_module = GetModule(self.geodesic_source+'#include "GeodesicODE.h"\n'
-			+self.cuda_date_modified,self.cuoptions)
-		geodesic_kernel = self.geodesic_module.get_function('GeodesicODE')
-
-		# Set the module constants
-		def SetCst(*args):cupy_module_helper.SetModuleConstant(self.geodesic_module,*args)
-		# Note: geodesic solver does not use bilevel array structure
-		shape_tot = self.shape
-		size_tot = int(np.prod(shape_tot))  #distinct from size_tot used for solver
-		SetCst('shape_tot',shape_tot,self.int_t)
-		SetCst('size_tot', size_tot, self.int_t)
-		typical_len = int(max(40,0.5*np.max(shape_tot)/geodesic_step))
-		typical_len = self.GetValue('geodesic_typical_length',default=typical_len,
-			help="Typical expected length of geodesics (number of points).")
-		# Typical geodesic length is max_len for the GPU solver, which computes just a part
-		SetCst('max_len', typical_len, self.int_t) 
-		causalityTolerance = self.GetValue('geodesic_causalityTolerance',default=4.,
-			help="Used in criterion for rejecting points in flow interpolation")
-		SetCst('causalityTolerance', causalityTolerance, self.float_t)
-		nGeodesics=len(self.tips)
-
-		# Prepare the euclidean distance to seed estimate (for stopping criterion)
-		xp = self.xp
-		eucl_bound = self.GetValue('geodesic_targetTolerance',default=6.,
-			help="Tolerance, in pixels, for declaring a seed as reached.")
-		eucl_t = geodesic_traits['EuclT']
-		eucl = np.zeros_like(self.seedTags,dtype=eucl_t)
-		eucl_integral = np.dtype(eucl_t).kind in ('i','u') # signed or unsigned integer
-		eucl_max = np.iinfo(eucl_t).max if eucl_integral else np.inf
-		eucl[np.logical_not(self.seedTags)] = eucl_max
-		eucl_mult = 5 if eucl_integral else 1
-		eucl_kernel = inf_convolution.distance_kernel(radius=1,ndim=self.ndim,
-			dtype=eucl_t,mult=eucl_mult)
-		eucl = inf_convolution.inf_convolution(eucl,eucl_kernel,
-			upper_saturation=eucl_max,overwrite=True,niter=int(np.ceil(eucl_bound)))
-		eucl[eucl>eucl_mult*eucl_bound] = eucl_max
-		eucl=xp.ascontiguousarray(eucl)
-
-		# Run the geodesic ODE solver
-		stopping_criterion = list(("Stopping criterion",)*nGeodesics)
-		corresp = list(range(nGeodesics))
-		tips = self.hfmIn.PointFromIndex(self.tips,to=True)
-		geodesics = [ [tip.reshape(1,-1)] for tip in tips]
-
-		block_size=self.GetValue('geodesic_block_size',default=32,
-			help="Block size for the GPU based geodesic solver")
-		values = xp.ascontiguousarray(self.hfmOut['values'].astype(self.float_t))
-		geodesic_termination_codes = [
-			'Continue', 'AtSeed', 'InWall', 'Stationnary', 'PastSeed', 'VanishingFlow']
-
-		max_len = int(max(40,20*np.max(shape_tot)/geodesic_step))
-		max_len = self.GetValue("geodesic_max_length",default=max_len,
-			help="Maximum allowed length of geodesics.")
-		
-		geoIt=0; geoMaxIt = int(np.ceil(max_len/typical_len))
-		while len(corresp)>0:
-			if geoIt>=geoMaxIt: 
-				self.Warn("Geodesic solver failed to converge, or geodesic has too many points"
-					" (in latter case, try setting 'geodesic_max_len':np.inf)")
-				break
-			geoIt+=1
-			nGeo = len(corresp)
-			x_s = xp.full( (nGeo,typical_len,self.ndim), np.nan, self.float_t)
-			x_s[:,0,:] = np.stack([geodesics[i][-1][-1,:] for i in corresp], axis=0)
-			len_s = xp.full((nGeo,),-1,self.int_t)
-			stop_s = xp.full((nGeo,),-1,np.int8)
-
-			nBlocks = int(np.ceil(nGeo/block_size))
-
-			SetCst('nGeodesics', nGeo, self.int_t)
-			geodesic_kernel( (nBlocks,),(block_size,),
-				(self.flow['flow_vector'],self.flow['flow_weightsum'],
-					values,eucl,x_s,len_s,stop_s))
-			corresp_next = []
-			for i,x,l,stop in zip(corresp,x_s,len_s,stop_s): 
-				geodesics[i].append(x[1:int(l)])
-				if stop!=0: stopping_criterion[i] = geodesic_termination_codes[int(stop)]
-				else: corresp_next.append(i)
-			corresp=corresp_next
-
-		geodesics_cat = [np.concatenate(geo,axis=0) for geo in geodesics]
-		self.hfmOut['geodesics']=[self.hfmIn.PointFromIndex(geo).T for geo in geodesics_cat]
-		self.hfmOut['geodesic_stopping_criteria'] = stopping_criterion
-
+	GetGeodesics = _GetGeodesics.GetGeodesics
+	
 	def FinalCheck(self):
 		self.hfmOut['keys']['unused'] = list(set(self.hfmIn.keys())-set(self.hfmOut['keys']['used']))
 		if self.verbosity>=1 and self.hfmOut['keys']['unused']:
