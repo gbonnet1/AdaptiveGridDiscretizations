06c77daf9b229e1b0d4121e3f4f9a86fb71e2d63
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-04-15 21:30:33 +0200

    Passing GPU tests

--- agd/AutomaticDifferentiation/cupy_support.py
+++ agd/AutomaticDifferentiation/cupy_support.py
@@ -14,15 +14,6 @@ def max(a,*args,**kwargs):
 	initial=kwargs.pop('initial') # cupy (old version ?) does not accept initial argument
 	return np.maximum(initial,np.max(a,*args,**kwargs))
 
-"""
-@implements(np.max,cupy_only=True)
-def max(a,*args,**kwargs):
-	try: return np.max(a,*args,**kwargs)
-	except TypeError: # cupy (old version ?) does not accept initial argument
-		initial = kwargs.pop('initial')
-		return np.maximum(initial,np.max(a,*args,**kwargs))
-"""
-
 def flat(a):
 	try: return a.flat # cupy.ndarray (old version ?) does not have flat
 	except AttributeError: return a.reshape(-1) 
@@ -32,33 +23,12 @@ def expand_dims(a,axis): # numpy will not accept cupy arrays
 	if axis<0: axis=1+a.ndim+axis
 	newshape = a.shape[:axis]+(1,)+a.shape[axis:]
 	return a.reshape(newshape)
-"""
-@implements(np.expand_dims,cupy_only=True)
-def expand_dims(a,axis):
-	try: return np.expand_dims(a,axis)
-	except TypeError: 
-		if axis<0: axis=1+a.ndim+axis
-		newshape = a.shape[:axis]+(1,)+a.shape[axis:]
-		return a.reshape(newshape)
-"""
 
 @implements_cupy_alt(np.full_like,TypeError)
 def full_like(arr,*args,**kwargs): # cupy (old version ?) lacks the shape argument
 	arr = np.broadcast_to(arr.flatten()[0], kwargs.pop('shape'))
 	return np.full_like(arr,*args,**kwargs)
 
-"""
-def _full_like(a,*args,**kwargs): # needed for cupy variant below
-	if is_ad(a): return type(a)(np.full_like(a.value,*args,**kwargs))
-	else: return np.full_like(a,*args,**kwargs) 
-
-@implements(np.full_like,cupy_only=True)
-def full_like(arr,*args,**kwargs):
-	try: return _full_like(arr,*args,**kwargs)
-	except TypeError: # Some old versions of cupy lack the shape argument
-		arr = np.broadcast_to(arr.flatten()[0], kwargs.pop('shape'))
-		return _full_like(arr,*args,**kwargs)
-"""
 def zeros_like(a,*args,**kwargs): return full_like(a,0.,*args,**kwargs)
 def ones_like(a,*args,**kwargs):  return full_like(a,1.,*args,**kwargs)
 
