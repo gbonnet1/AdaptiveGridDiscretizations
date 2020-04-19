7af50d63dc92c5c9cacbebe22b12bbe44ce41fe3
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-04-19 22:08:49 +0200

    Working on GPU TTI

--- agd/HFMUtils/HFM_CUDA/cuda/TTI_.h
+++ agd/HFMUtils/HFM_CUDA/cuda/TTI_.h
@@ -45,6 +45,7 @@ Scalar det_vv(const Scalar x[ndim], const Scalar y[ndim]){
 
 /// Returns smallest positive s such that 0.5*q*s^2 + l*s -1 = 0
 Scalar solve(const Scalar l, const Scalar q){
+	// TODO : use conjugate formulas to guarantee stability when q vanishes
 	if(q==0){return 1/l;}
 	const ScalarType delta = l*l + 2.*q;
 	const ScalarType sdelta = sqrt(delta);
@@ -73,7 +74,7 @@ Scalar multiplier(const Scalar linear[ndim], const Scalar quadratic[symdim],
 
 	const Scalar Q[symdim] = { // comatrix of quadratic
 		coef_m(quadratic,1,1),-coef_m(quadratic,0,1),coef_m(quadratic,0,0)};
-	const Sym2 Q = quadratic.Comatrix();
+//	const Sym2 Q = quadratic.Comatrix();
 	const Scalar * l = linear; // l[2]
 	Scalar Ql[ndim]; dot_mv(Q,l,Ql);
 	const Scalar detQ = det_m(Q);
@@ -98,7 +99,7 @@ Scalar multiplier(const Scalar linear[ndim], const Scalar quadratic[symdim],
 
 // scheme returns mix_is_min
 bool scheme(const Scalar geom[geom_size], 
-	Scalar weights[ntotx], Int offsets[ntotx][ndim]){
+	Scalar weights[nactx], Int offsets[nactx][ndim]){
 	const Scalar * linear = geom; // linear[2]
 	const Scalar * quadratic = geom + 2; // quadratic[dim2::symdim]
 	const Scalar * transform = geom + (2+dim2::symdim); // transform[ndim * ndim]
@@ -117,6 +118,86 @@ bool scheme(const Scalar geom[geom_size],
 		for(Int i=0; i<symdim; ++i) {
 			D[i] = mult*((Scalar(1)-slope)*D0[i] + slope*D1[i]);} // TODO : check
 		selling_m(D, weights+kmix*symdim, offsets + kmix*symdim);
+	}
+	return mix_is_min;
+}
+
+
+/*Returns the two roots of a quadratic equation, a t^2 + 2 b t + c t^2.
+The discriminant must be non-negative, but aside from that the equation may be degenerate*/
+void solve2(const Scalar a, const Scalar b, const Scalar c, Scalar r[2]){
+	const Scalar sdelta = sqrt(b*b-a*c);
+	const u = - b + sdelta, v = -sdelta-b;
+	if(abs(a)>abs(c)) {r[0] = u/a; r[1] = v/a;        return;}
+	else if(c!=0)     {r[0] = c/u; r[1] = c/v;        return;}
+	else              {r[0] = 0;   r[1] = infinity(); return;}
+}
+
+/*Returns the smallest root of the considered quadratic equation above the given threshold.
+Such a root is assumed to exist.*/
+Scalar solve2_above(const Scalar a, const Scalar b, const Scalar c, const Scalar above){
+	Scalar r[2]; solve2(a,b,c,r);
+	const bool ordered = r[0]<r1;
+	const Scalar rmin = ordered ? r[0] : r[1], rmax = ordered ? r[1] : r[0];
+	return rmin>=above ? rmin : rmax;
+}
+
+namespace dim2 {
+
+/** Returns df(x)/<x,df(x)> where f(x):= C + 2 <l,x> + <qx,x> */
+void grad_ratio(const Scalar l[2], const Scalar q[3], const Scalar x[2], Scalar g[2]){
+		Scalar hgrad[2]; dot_mv(q,x,hgrad); add_vV(l,hgrad); // df(x)/2
+		const Scalar xhg = scal_vv(x,hgrad);
+		g[0]=hgrad[0]/xhg; g[1]=hgrad[1]/xhg;
+}
+
+/** Samples the curve defined by f(x)=0,x>=0, 
+where f(x):= -2 + 2 <l,x> + <qx,x>,
+and returns diag(i) := .*/
+bool SetDiags(const Scalar l[2],const Scalar q[3],diag_s[nmix][2]){
+	// Equation is <l,x> + 0.5 <x,q,x> = 1
+	const Scalar a = solve2_above(-2,l[0],q[0],0.); // (a,0) is on the curve
+	const Scalar b = solve2_above(-2,l[1],q[2],0.); // (0,b) is on the curve
+
+	// Change of basis 
+//	const Scalar is2 = 1/sqrt(2);
+	const Scalar e0[2] = {1/2.,1/.2}, e1={1/2.,-1/2.};
+	const Scalar L[2] = {scal_vv(l,e0),scal_vv(l,e1)};
+	const Scalar Q[3] = {scal_vmv(e0,q,e0),scal_vmv(e0,q,e1),scal_vmv(e1,q,e1)};
+
+	Scalar x_s[nmix][2]; // Curve sampling
+	x_s[0]={a,0}; x_s[nmix-1]={0,b};
+	for(Int i=1;i<nmix-1; ++i){
+		const Scalar t = i/Scalar(nmix-1);
+		const Scalar v = (1-t)*a - t*b;
+		// Solving f(u e0+ v e_1) = 0 w.r.t u
+		const Scalar u = solve_above(-2+2*L[1]*v+Q[2]*v*v,
+			L[0]+Q[1]*v,Q[0],abs(v));
+		// Inverse change of basis
+		x[i] = {u+v,u-v};
+	} 
+	for(Int i=0; i<nmin; ++i){grad_ratio(l,q,x_s[i],diag_s[i]);}
+}
+
+/* Alternative implementation*/
+bool Scheme(const const Scalar geom[geom_size], 
+	Scalar weights[nactx], Int offsets[nactx][ndim]){
+	const Scalar * linear = geom; // linear[2]
+	const Scalar * quadratic = geom + 2; // quadratic[dim2::symdim]
+	const Scalar * transform = geom + (2+dim2::symdim); // transform[ndim * ndim]
+
+	Scalar diag_s[nmix][2];
+	const bool mix_is_min = SetDiags(linear,quadratic,diag_s);
+	Scalar D0[symdim]; self_outer_v(transform,D0);
+	Scalar D1[symdim]; self_outer_v(transform+ndim,D1);
+	if(ndim==3){Scalar D2[symdim]; self_outer_v(transform+2*ndim,D2);
+		for(Int i=0; i<symdim; ++i) D1[i]+=D2[i];}
+
+	for(Int kmix=0; kmix<nmix; ++kmix){
+		const Scalar * diag = diag_s[kmix]; // diag[2];
+		Scalar D[symdim];
+		for(Int i=0; i<symdim; ++i) {D[i] = diag[0]*D0[i] + diag[1]*D1[i];} 
+		selling_m(D, weights+kmix*symdim, offsets+kmix*symdim);
 	}
 	return mix_is_min;
 }
