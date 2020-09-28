97a9b595711ca8f913eabf8b741248dd83dfa76c
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2020-09-28 16:01:57 +0200

    Working on rolling ball

---
+++ agd/ExportedCode/Notebooks_GPU/Trailers.py
@@ -0,0 +1,8 @@
+# Code automatically exported from notebook Trailers.ipynb in directory Notebooks_GPU
+# Do not modify
+from ... import Eikonal
+from ... import AutomaticDifferentiation as ad
+from ... import Metrics
+from ... import LinearParallel as lp
+norm_infinity = ad.Optimization.norm_infinity
+
